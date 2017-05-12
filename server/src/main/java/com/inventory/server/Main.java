fc38da6474fef6ea037e399034b8390bdb77bb32
StoyanTenev <stoian88lz@gmail.com>
2017-05-12 19:47:59 +0300

    Add spring dependency

--- server/src/main/java/com/inventory/server/Main.java
+++ server/src/main/java/com/inventory/server/Main.java
@@ -1,4 +1,13 @@
 package com.inventory.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
 }
