321d7169cf030ba28ab400f628d7ae3b88e0d8ad
StoyanTenev <stoian88lz@gmail.com>
2017-06-01 21:41:22 +0300

    I refactored the code to keep single responsibility principle.

--- server/src/main/java/com/inventory/server/product/ProductService.java
+++ server/src/main/java/com/inventory/server/product/ProductService.java
@@ -4,6 +4,8 @@ package com.inventory.server.product;
 
 import com.inventory.server.product.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,5 +20,19 @@ public class ProductService {
     public List<Product> getAllProducts() {
 
         return productRepository.findAll();
+    }
+
+    public Product getProductById(Integer id) throws Exception {
+        if (!this.productRepository.exists(id)) {
+            throw new Exception("Product with this ID not exist.");
+        }
+
+        return this.productRepository.findOne(id);
+    }
+
+    public ResponseEntity<Product> createProduct(Product product) {
+
+        productRepository.save(product);
+        return new ResponseEntity<>(product, HttpStatus.OK);
     }
 }
--- server/src/main/java/com/inventory/server/product/rest/Controller.java
+++ server/src/main/java/com/inventory/server/product/rest/Controller.java
@@ -1,11 +1,8 @@
 package com.inventory.server.product.rest;
 
-import com.google.common.base.Preconditions;
-import com.inventory.server.product.ProductRepository;
 import com.inventory.server.product.ProductService;
 import com.inventory.server.product.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,30 +15,19 @@ public class Controller {
     @Autowired
     private ProductService productService;
 
-    @Autowired
-    private ProductRepository productRepository;
-
     @RequestMapping(path = "/products", method = RequestMethod.GET)
     public List<Product> getAll() {
         return productService.getAllProducts();
     }
 
     @RequestMapping(path = "product/{id}", method = RequestMethod.GET)
-    public Product getProduct(@PathVariable Integer id) {
-        if (!this.productRepository.exists(id)) {
-            return null;
-        }
-
-        Product product = this.productRepository.findOne(id);
-
-        return product;
+    public Product getProduct(@PathVariable Integer id) throws Exception {
+        return productService.getProductById(id);
     }
 
-    @RequestMapping(value = "product/create", method = RequestMethod.POST)
-    public ResponseEntity<Product> update(@RequestBody Product product) {
-
-        productRepository.save(product);
-        return new ResponseEntity<>(product, HttpStatus.OK);
+    @RequestMapping(value = "/products", method = RequestMethod.POST, produces = "application/json")
+    public ResponseEntity<Product> create(@RequestBody Product product) {
+        return productService.createProduct(product);
     }
 
 }
