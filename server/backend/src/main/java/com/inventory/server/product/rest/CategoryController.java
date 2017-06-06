fd80bbf7d94d368d32cee1237a9384bea8906a9c
Zhivko Delchev <Jako8809@gmail.com>
2017-06-06 21:19:46 +0200

    rename category controller

--- server/backend/src/main/java/com/inventory/server/product/rest/CategoryController.java
+++ server/backend/src/main/java/com/inventory/server/product/rest/CategoryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class CategoryControler {
+public class CategoryController {
 
     @Autowired
     ProductService productService;
