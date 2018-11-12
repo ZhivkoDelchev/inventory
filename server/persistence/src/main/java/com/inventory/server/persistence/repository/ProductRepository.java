4497c7594f9be9d228cab67ff571841db33dc076
Zhivko Delchev <zhivko.delchev@tis.biz>
2018-11-12 08:41:23 +0200

    add missing annotation.

--- server/persistence/src/main/java/com/inventory/server/persistence/repository/ProductRepository.java
+++ server/persistence/src/main/java/com/inventory/server/persistence/repository/ProductRepository.java
@@ -4,10 +4,11 @@ import com.inventory.server.persistence.model.Product;
 import com.inventory.server.persistence.dto.Category;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+@Repository
 public interface ProductRepository extends CrudRepository<Product, Integer> {
     List<Product> findAll();
 
